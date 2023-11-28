eeb9af0b0b93c38badf407a8d0aec92c6d2a91a6
PSEERVERANCE\晒sensei的suputer <54572905@qq.com>
2023-11-28 20:58:07 +0800

    函数参数、界面基本使用、文字、布局、背景颜色

--- flutter-projects/first_app/lib/main.dart
+++ flutter-projects/first_app/lib/main.dart
@@ -1,5 +1,15 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-void main(){
-  runApp(app);
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        // backgroundColor: Color.fromARGB(1, 2, 3, 42),
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    ),
+  );
 }
