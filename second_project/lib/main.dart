e69ae336b13e2b35d486b204b4671451fe8faf8b
Sonder <108516200+BocaiheDabaicai@users.noreply.github.com>
2025-07-18 15:18:40 +0800

    项目安装及配置

--- second_project/lib/main.dart
+++ second_project/lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Pag31212da'),
+      home: const MyHomePage(title: 'Fluttertest 1212da'),
     );
   }
 }
